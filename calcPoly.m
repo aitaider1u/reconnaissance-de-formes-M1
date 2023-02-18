e104ba2ae4d959dd1c94fe051b13739f6e1580e2
zinedine <zine>
2023-02-18 23:44:39 +0100

    clean code

--- calcPoly.m
+++ calcPoly.m
@@ -1,20 +1,17 @@
 function [x,y] = calcPoly(m,t,img)
     h = size(img,1);
     w = size(img,2);
-    dist = 1 ; %rayon 
-    x = m(1);    %   cos 
-    y = m(2);    %   sin 
-    try 
-        while  all([x <=w, x >= 1, y <= h, y >= 1,img(y,x) == 1])
-                dist = dist+1;
-                x = floor( m(1) + dist*sin(t));
-                y = floor( m(2) - dist*cos(t));   
-        end
-    catch
-         x = floor( m(1) + (dist-1)*sin(t));
-         y = floor( m(2) - (dist-1)*cos(t));   
-        return
-    end 
-         x = floor( m(1) + (dist-1)*sin(t));
-         y = floor( m(2) - (dist-1)*cos(t));   
+    dist = 1; %rayon 
+    x = m(1);     
+    y = m(2);     
+    while  all([x <=w, x >= 1, y <= h, y >= 1])
+        if (img(y,x) == 0 )
+            break
+        end 
+        dist = dist+1;
+        x = floor( m(1) + dist*sin(t));
+        y = floor( m(2) - dist*cos(t));   
+    end
+    x = floor( m(1) + (dist-1)*sin(t));
+    y = floor( m(2) - (dist-1)*cos(t));   
 end 
